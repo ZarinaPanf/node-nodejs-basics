001bd48ff9823fff98094e8b423c18c3621682fa
Zarina Panfilova <zhiyenbayeva.z@gmail.com>
2024-01-25 23:40:20 +0600

    init: add create.js, read.js and copy.js files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import fs from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+  fs.cp('./files', './files_copy', { recursive: true }, (err) => {
+    if (err) {
+      console.error('FS operation failed');
+    }
+  });
 };
 
 await copy();
