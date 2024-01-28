7a2daddf04d9d57e003a6a4e50a60fb8c1087565
Zarina Panfilova <zhiyenbayeva.z@gmail.com>
2024-01-28 21:40:27 +0600

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import crypto from 'crypto';
+
+const secret = 'secretkey';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hmac = crypto.createHmac('sha256', secret);
+    const fileStream = fs.createReadStream('./files/fileToCalculateHashFor.txt');
+
+    for await (const chunk of fileStream) {
+        hmac.update(chunk);
+    }
+
+    const fileHmac = hmac.digest('hex');
+    console.log(`SHA256 HMAC of the file: ${fileHmac}`);
 };
 
 await calculateHash();
